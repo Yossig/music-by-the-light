e493fb3e4adaaa321720ffb3d748bd0f2d8abb0d
Yossig <yosigartner@gmail.com>
2020-11-07 02:58:54 +0200

    add arduino code

---
+++ iot/arduino-read-sensor.c
@@ -0,0 +1,21 @@
+int sensorPin = A0; // select the input pin for LDR
+
+int sensorValue = 0; // variable to store the value coming from the sensor
+void setup() {
+Serial.begin(9600); //sets serial port for communication
+}
+
+int singleSampleInterval = 100;
+int meanSamplesInterval = 1000;
+int intervalCounter = 0;
+int totalValues = 0;
+void loop() {
+totalValues += analogRead(sensorPin);
+delay(singleSampleInterval);
+intervalCounter+=singleSampleInterval;
+if(intervalCounter >= meanSamplesInterval) {
+  Serial.println(totalValues / (meanSamplesInterval / singleSampleInterval));
+  intervalCounter = 0;
+  totalValues = 0;
+}
+}
